7d8764e257870a3bf229ab805fe7902f8b5afe4a
[Git] <[opunchak@gmail.com]>
2017-04-06 14:58:07 +0300

    Second commit

---
+++ Artifactid/src/main/java/cat/main/academy/ua/MyFirstClass.java
@@ -0,0 +1,13 @@
+package cat.main.academy.ua;
+import java.util.Scanner;
+/**
+ * Created by 450G1 on 04.04.2017.
+ */
+public class MyFirstClass {
+public static void main(String[] arg){
+    System.out.println("Hello Brain Academy!");
+    Scanner sc=new Scanner(System.in);
+    String line=sc.nextLine();
+    System.out.println(line);
+}
+}
